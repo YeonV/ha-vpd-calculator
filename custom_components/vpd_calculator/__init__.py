3dc2099b7309abb459f170de2f685d638928d379
YeonV <trusted.yeon@gmail.com>
2025-04-20 04:22:41 +0200

    drastic test

--- custom_components/vpd_calculator/__init__.py
+++ custom_components/vpd_calculator/__init__.py
@@ -1,45 +1,15 @@
-"""The VPD Calculator integration."""
-from __future__ import annotations
-
+# TEMPORARY TEST __init__.py
 import logging
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
-# from homeassistant.const import Platform
-
-from .const import DOMAIN
-from .mqtt_publisher import VPDCalculatorMqttPublisher
 
 _LOGGER = logging.getLogger(__name__)
 
-# Define the platform this integration creates entities for
-# PLATFORMS: list[Platform] = [Platform.SENSOR]
-
-
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Set up VPD Calculator from a config entry."""
-    _LOGGER.info("Setting up VPD Calculator entry %s (MQTT)", entry.entry_id)
-
-    # Create and store the publisher instance for this entry
-    publisher = VPDCalculatorMqttPublisher(hass, entry)
-    hass.data.setdefault(DOMAIN, {})[entry.entry_id] = publisher
-
-    # Start the publisher's setup (MQTT discovery, listeners)
-    await publisher.async_setup()
-
+    _LOGGER.error("!!! TEST: async_setup_entry in vpd_calculator WAS CALLED !!!")
+    # Don't actually set anything up, just prove it runs
     return True
 
-
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    """Unload a config entry."""
-    _LOGGER.info("Unloading VPD Calculator entry %s (MQTT)", entry.entry_id)
-
-    publisher = hass.data[DOMAIN].get(entry.entry_id)
-    unload_ok = False
-    if publisher:
-        unload_ok = await publisher.async_unload() # Tell publisher to clean up
-
-    if unload_ok:
-        hass.data[DOMAIN].pop(entry.entry_id, None) # Remove from hass.data
-
-    return unload_ok
+    _LOGGER.error("!!! TEST: async_unload_entry in vpd_calculator WAS CALLED !!!")
+    return True
